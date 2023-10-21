466515da7961ae679187c3a005f93e7fba366b65
Isaac Finotti <imisaac2807@gmail.com>
2023-10-21 16:05:13 -0300

    Creating a 404 page

--- src/main.tsx
+++ src/main.tsx
@@ -8,6 +8,7 @@ import { About } from "./components/About/index.tsx";
 import { Menu } from "./components/Menu/index.tsx";
 import { Posts } from "./components/Post/index.tsx";
 import { Redirect } from "./components/Redirect/index.tsx";
+import { NotFound } from "./components/NotFound/index.tsx";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
@@ -19,6 +20,7 @@ ReactDOM.createRoot(document.getElementById("root")!).render(
         <Route path="/posts/:id" element={<Posts />} />
         <Route path="/posts" element={<Posts />} />
         <Route path="/redirect" element={<Redirect />} />
+        <Route path="*" element={<NotFound />} />
       </Routes>
     </BrowserRouter>
   </React.StrictMode>,
