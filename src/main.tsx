cbf7caa54c1387bfb60ddf48f11fccf7823832b7
Isaac Finotti <imisaac2807@gmail.com>
2023-10-21 15:50:21 -0300

    Redirecting pages with navigate

--- src/main.tsx
+++ src/main.tsx
@@ -7,6 +7,7 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import { About } from "./components/About/index.tsx";
 import { Menu } from "./components/Menu/index.tsx";
 import { Posts } from "./components/Post/index.tsx";
+import { Redirect } from "./components/Redirect/index.tsx";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
@@ -17,6 +18,7 @@ ReactDOM.createRoot(document.getElementById("root")!).render(
         <Route path="/about" element={<About />} />
         <Route path="/posts/:id" element={<Posts />} />
         <Route path="/posts" element={<Posts />} />
+        <Route path="/redirect" element={<Redirect />} />
       </Routes>
     </BrowserRouter>
   </React.StrictMode>,
