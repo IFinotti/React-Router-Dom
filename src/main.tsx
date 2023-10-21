9e32f0bc86da06e40a2705be5693266e06de051a
Isaac Finotti <imisaac2807@gmail.com>
2023-10-20 22:35:37 -0300

    LEARNING ABOUT THE DIFFERENCE BETWEEN LINK AND A

--- src/main.tsx
+++ src/main.tsx
@@ -5,10 +5,12 @@ import { Home } from "./components/Home/index.tsx";
 
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import { About } from "./components/About/index.tsx";
+import { Menu } from "./components/Menu/index.tsx";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
     <BrowserRouter>
+      <Menu />
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/about" element={<About />} />
